4c1b881440a7aa447389a262a3793dad000a1b9c
abiwinanda <abiwinanda@outlook.com>
2022-08-14 09:55:23 +0800

    add typespec to bug 5

--- lib/bug_5.ex
+++ lib/bug_5.ex
@@ -1,8 +1,11 @@
 defmodule Cashy.Bug5 do
+  @type currency() :: :sgd | :usd
+  @spec convert(currency, currency, number) :: number
   def convert(:sgd, :usd, amount) do
     amount * 0.70
   end
 
+  @spec amount({:value, number}) :: number
   def amount({:value, value}) do
     value
   end
